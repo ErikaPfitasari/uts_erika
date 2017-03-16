6a60f3050d48439853463fbf36a0943f4085585a
erikaprafitasari <erikaprafitasari1@gmail.com>
2017-03-16 21:38:28 +0700

    iniuts

---
+++ src/utserika/Utserika.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utserika;
+
+/**
+ *
+ * @author ERIKA-MOKLET
+ */
+public class Utserika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
